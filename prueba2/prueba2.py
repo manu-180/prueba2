13095468b43fbcf1d098622bec1bef6244724b7c
manu-180 <manumanu97@hotmail.com>
2024-05-31 01:36:35 -0300

    a

--- prueba2/prueba2.py
+++ prueba2/prueba2.py
@@ -38,7 +38,9 @@ dotenv.load_dotenv()
 DATABASE_URL = os.environ.get("DATABASE_URL")
 GOOGLE_APPLICATION_CREDENTIALS = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
 
-firebase_sdk = credentials.Certificate(f"C:\\Users\\Manuel\\Desktop\\Folder\\prueba2\\{GOOGLE_APPLICATION_CREDENTIALS}")
+firebase_sdk = credentials.Certificate(
+    #f"C:\\Users\\Manuel\\Desktop\\Folder\\prueba2\\
+        GOOGLE_APPLICATION_CREDENTIALS)
 firebase_admin.initialize_app(firebase_sdk, {"databaseURL": DATABASE_URL})
 
 class FireBase():
