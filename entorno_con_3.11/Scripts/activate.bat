7719c2f71dd05839fd06f9573e5ee7b425f71478
manu-180 <manumanu97@hotmail.com>
2024-06-01 00:23:20 -0300

    d

--- entorno_con_3.11/Scripts/activate.bat
+++ entorno_con_3.11/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Manuel\Desktop\Folder\prueba2\entorno_con_3.11
+set VIRTUAL_ENV=C:\users\manuel\desktop\folder\prueba2\entorno_con_3.11
 
 if not defined PROMPT set PROMPT=$P$G
 
